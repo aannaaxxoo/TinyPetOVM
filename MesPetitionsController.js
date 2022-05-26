7adb0483b09f4e35538a48b9c777b81082d18e5a
aannaaxxoo <oxana.bernardleroux@gmail.com>
2022-05-26 16:18:23 +0200

    visuel fini

--- MesPetitionsController.js
+++ MesPetitionsController.js
@@ -60,13 +60,18 @@
             dateCrea: moment().format('L') //date du jour au format JJ/MM/AAAA
 		};
 
-        //a récuperer depuis le back
+        //a récuperer depuis le back en donnant l'id de l'auteur
         vm.listePetitions = [
             {titre:'titre3',description : 'description3',image:'image3',objectif:'3',auteur:'moi',dateCrea:'03/03/2022',nbSignature:3},
             {titre:'titre2',description : 'description2',image:'image2',objectif:'2',auteur:'auteur2',dateCrea:'02/02/2022',nbSignature:2},
             {titre:'titre4',description : 'description4',image:'image4',objectif:'4',auteur:'moi',dateCrea:'04/04/2022',nbSignature:4},
-            {titre:'titre1',description : 'description1',image:'image1',objectif:'1',auteur:'auteur1',dateCrea:'01/01/2022',nbSignature:1}
-            
+            {titre:'titre1',description : 'description1',image:'image1',objectif:'1',auteur:'auteur1',dateCrea:'01/01/2022',nbSignature:1} 
+        ];
+
+		//a récuperer depuis le back en donnant l'id de l'auteur
+		vm.listePetitionsSignees = [
+            {titre:'titre4',description : 'description4',image:'image4',objectif:'4',auteur:'moi',dateCrea:'04/04/2022',nbSignature:4, dateSignature:'25/06/22'},
+            {titre:'titre1',description : 'description1',image:'image1',objectif:'1',auteur:'auteur1',dateCrea:'01/01/2022',nbSignature:1, dateSignature:'28/06/22'}
         ];
     }
 }());
